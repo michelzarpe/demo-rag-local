86ec9621cb2e0b6910b45d5c380f2af525f7446b
Giuliana Silva Bezerra - DATAPREVSC <giuliana.bezerra@dataprev.gov.br>
2025-12-20 16:44:44 -0300

    final commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# TODO Implementar
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.rag import ask_rag
+
+app = FastAPI()
+
+class Question(BaseModel):
+    question: str
+
+@app.post("/pergunta")
+def perguntar(data: Question):
+    answer = ask_rag(data.question)
+    return {"answer": answer}
--- app/rag.py
+++ app/rag.py
@@ -1,1 +1,34 @@
-# TODO Implementar
+from langchain_chroma import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms import Ollama
+
+CHROMA_PATH = "chroma"
+
+def ask_rag(question: str):
+    embeddings = OllamaEmbeddings(
+        model="nomic-embed-text"
+    )
+
+    db = Chroma(
+        persist_directory=CHROMA_PATH,
+        embedding_function=embeddings
+    )
+
+    docs = db.similarity_search(question, k=4)
+
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    prompt = f"""
+Use apenas o contexto abaixo para responder a pergunta.
+Se a resposta não estiver no contexto, diga que não encontrou a informação.
+
+Contexto:
+{context}
+
+Pergunta:
+{question}
+"""
+
+    llm = Ollama(model="mistral")
+
+    return llm.invoke(prompt)
